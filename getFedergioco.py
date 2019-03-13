d6f98f32ca0f1509fea199bbcdc6eb80c978bbbb
mr.curk <mr.curk@gmail.com>
2019-03-13 23:42:49 +0100

    add config file

--- getFedergioco.py
+++ getFedergioco.py
@@ -1,6 +1,9 @@
+import json
+import sys
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
 
 
 ########################################################################################################################
@@ -39,7 +42,6 @@ def toMonthYear(month, year):
 ########################################################################################################################
 ### function to convert string € value to float
 def euroString2Float(value):
-
     # remove €
     data = value.replace(' €', '')
     # remove . from data
@@ -112,8 +114,8 @@ def fetchCasinoMonthData(month, year):
             if i == 0:
                 # first column is game name
                 gameName = singleColumn.text
-                if gameName=="INGRESSI (numero)":
-                    gameName="INGRESSI"
+                if gameName == "INGRESSI (numero)":
+                    gameName = "INGRESSI"
             else:
                 # others columns are data
                 casinos[i - 1].data[gameName] = euroString2Float(singleColumn.text)
@@ -123,14 +125,68 @@ def fetchCasinoMonthData(month, year):
 
 
 ########################################################################################################################
+# string to boolean
+def str_to_bool(s):
+    if s.upper() == 'TRUE':
+        return True
+    elif s.upper() == 'FALSE':
+        return False
+    else:
+        raise ValueError
+
+
+########################################################################################################################
 # PROGRAM
 ########################################################################################################################
 ##  CONFIG PARAMETERS - geting data from config file
+## full path of config file
+if len(sys.argv) > 1:
+    config_full_path = sys.argv[1]
+else:
+    config_full_path = None
 
+## read config file
+if config_full_path != None:
+    config_json = open(config_full_path).read()
+else:
+    config_json = open("config.json").read()
+
+config_data = json.loads(config_json)
+
+## config proxy
+# get proxy data from config
+proxyType = config_data["PROXY_TYPE"]
+proxy = config_data["PROXY"]
+proxyPort = config_data["PROXY_PORT"]
+proxyUsername = config_data["PROXY_USERNAME"]
+proxyPassword = config_data["PROXY_PASSWORD"]
+proxyString = None
+# set proxy string
+if len(proxyType) > 0:
+    if len(proxyUsername) == 0 and len(proxyPassword) == 0:
+        proxyString = proxyType + "://" + proxy + ":" + proxyPort
+    else:
+        proxyString = proxyType + "://" + proxyUsername + ":" + proxyPassword + "@" + proxy + ":" + proxyPort
+    # set proxy
+    os.environ['http_proxy'] = proxyString
+    os.environ['HTTP_PROXY'] = proxyString
+    os.environ['https_proxy'] = proxyString
+    os.environ['HTTPS_PROXY'] = proxyString
+
+# test mode, without db connection == True
+test_mode_no_db = str_to_bool(config_data["test_mode_no_db"])
+
+# connection string
+dbTns = config_data["DB_TNS"]
+connection_string = None
+if len(dbTns) == 0:
+    connection_string = config_data["DB_USERNAME"] + "/" + config_data["DB_PASSWORD"]
+else:
+    connection_string = config_data["DB_USERNAME"] + "/" + config_data["DB_PASSWORD"] + "@" + config_data["DB_TNS"]
 
 ########################################################################################################################
 # generate load id
 loadId = generateLoadId()
-casinos = fetchCasinoMonthData(11, 2018)
+casinos = fetchCasinoMonthData(1, 2019)
 for item in casinos:
     item.printData()
